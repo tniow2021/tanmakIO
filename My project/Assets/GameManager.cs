7535bb726cbe57fb44df9b73e42d4b6cb25c0755
tniow2021 <85551224+tniow2021@users.noreply.github.com>
2024-01-07 12:37:41 +0900

    도메인 추가

--- My project/Assets/GameManager.cs
+++ My project/Assets/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager
     {
         if (Instance is not null) return;
         Instance = this;
-        IPAddress ServerIP = IPAddress.Loopback;
+        IPAddress ServerIP = Dns.GetHostAddresses("www.tempdomain123.shop")[0];
         int port = 2024;
         //네트워크 객체에 타입버퍼 객체를 넣어준다.
         network = new ClientNetwork(ServerIP, port,new TypeBuff());
